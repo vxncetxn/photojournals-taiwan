d393589dabc2217b8ed1741a128ac8013b3bb1f1
Vance <thevancetan@gmail.com>
2019-08-21 10:32:41 +0800

    fixed all cursor style alignment and layout issues

--- src/components/Cursor.js
+++ src/components/Cursor.js
@@ -47,7 +47,6 @@ const CursorLabel = styled.span`
   position: fixed;
   top: 0;
   left: 0;
-  display: none;
   width: 200px;
   text-align: center;
   font-family: var(--font-secondary), sans-serif;
@@ -56,6 +55,7 @@ const CursorLabel = styled.span`
   text-transform: uppercase;
   color: #e6e6e6;
   transform-origin: 50% 485%;
+  z-index: 999;
 
   //   &::after {
   //     position: absolute;
@@ -79,7 +79,14 @@ const CursorIcon = styled.span`
   position: fixed;
   top: 0;
   left: 0;
-  display: none;
+
+  box-sizing: border-box;
+  width: 50px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
   font-family: var(--font-secondary), sans-serif;
   font-size: 32px;
   font-weight: 700;
@@ -102,16 +109,16 @@ const CursorComp = ({ popped, cursorColor }) => {
       document.querySelector(".cursor-label").style.left = `${e.clientX -
         101}px`;
       document.querySelector(".cursor-label").style.top = `${e.clientY - 90}px`;
-
-      document.querySelector(".cursor-icon").style.left = `${e.clientX - 12}px`;
-      document.querySelector(".cursor-icon").style.top = `${e.clientY - 21}px`;
+      document.querySelector(".cursor-icon").style.left = `${e.clientX - 25}px`;
+      document.querySelector(".cursor-icon").style.top = `${e.clientY -
+        23.5}px`;
     });
 
     document.querySelectorAll(".gatsby-image-wrapper").forEach(node => {
       node.addEventListener("mouseenter", () => {
         document.querySelector(".cursor").style.clipPath = "circle(50%)";
         document.querySelector(".cursor-label").innerHTML = "View Image";
-        document.querySelector(".cursor-label").style.display = "block";
+
         document.querySelector(".cursor-icon").innerHTML = `<svg
             enableBackground="new 0 0 488.85 488.85"
             viewBox="0 0 488.85 488.85"
@@ -121,14 +128,11 @@ const CursorComp = ({ popped, cursorColor }) => {
           >
             <path d="m244.425 98.725c-93.4 0-178.1 51.1-240.6 134.1-5.1 6.8-5.1 16.3 0 23.1 62.5 83.1 147.2 134.2 240.6 134.2s178.1-51.1 240.6-134.1c5.1-6.8 5.1-16.3 0-23.1-62.5-83.1-147.2-134.2-240.6-134.2zm6.7 248.3c-62 3.9-113.2-47.2-109.3-109.3 3.2-51.2 44.7-92.7 95.9-95.9 62-3.9 113.2 47.2 109.3 109.3-3.3 51.1-44.8 92.6-95.9 95.9zm-3.1-47.4c-33.4 2.1-61-25.4-58.8-58.8 1.7-27.6 24.1-49.9 51.7-51.7 33.4-2.1 61 25.4 58.8 58.8-1.8 27.7-24.2 50-51.7 51.7z" />
           </svg>`;
-        document.querySelector(".cursor-icon").style.display = "block";
       });
       node.addEventListener("mouseleave", () => {
         document.querySelector(".cursor").style.clipPath = "circle(30%)";
         document.querySelector(".cursor-label").innerHTML = null;
-        document.querySelector(".cursor-label").style.display = "none";
         document.querySelector(".cursor-icon").innerHTML = null;
-        document.querySelector(".cursor-icon").style.display = "none";
       });
     });
 
@@ -136,16 +140,15 @@ const CursorComp = ({ popped, cursorColor }) => {
       node.addEventListener("mouseenter", () => {
         document.querySelector(".cursor").style.clipPath = "circle(50%)";
         document.querySelector(".cursor-label").innerHTML = "Visit Link";
-        document.querySelector(".cursor-label").style.display = "block";
-        document.querySelector(".cursor-icon").innerHTML = "&rarr;";
-        document.querySelector(".cursor-icon").style.display = "block";
+        document.querySelector(
+          ".cursor-icon"
+        ).innerHTML = `<span style={{ marginBottom: "7px" }}>&rarr;</span>`;
       });
       node.addEventListener("mouseleave", () => {
         document.querySelector(".cursor").style.clipPath = "circle(30%)";
         document.querySelector(".cursor-label").innerHTML = null;
-        document.querySelector(".cursor-label").style.display = "none";
+
         document.querySelector(".cursor-icon").innerHTML = null;
-        document.querySelector(".cursor-icon").style.display = "none";
       });
     });
 
@@ -153,25 +156,22 @@ const CursorComp = ({ popped, cursorColor }) => {
       node.addEventListener("mouseenter", () => {
         document.querySelector(".cursor").style.clipPath = "circle(50%)";
         document.querySelector(".cursor-label").innerHTML = "Change Cursor";
-        document.querySelector(".cursor-label").style.display = "block";
         document.querySelector(".cursor-icon").innerHTML = `<svg
-          enableBackground="new 0 0 383.344 383.345"
-          height="383.345"
-          viewBox="0 0 383.344 383.345"
-          width="20px"
-          fill="#fcfcfc"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="m273.217 106.899c-27.181-44.864-57.413-83.693-73.016-102.846-2.088-2.565-5.221-4.054-8.528-4.053-3.308 0-6.44 1.489-8.529 4.054-15.602 19.159-45.834 58.001-73.015 102.869-35.028 57.823-52.789 105.63-52.789 142.09 0 74.071 60.261 134.332 134.332 134.332s134.332-60.261 134.332-134.332c.001-36.484-17.758-84.298-52.787-142.114zm-63.111 226.969c-7.844 2.006-15.986 3.022-24.205 3.022-50.186 0-91.015-37.929-91.015-84.55 0-11.255 2.97-24.405 8.825-39.083.989-2.48 3.807-3.895 6.585-3.295 2.776.598 4.64 3.018 4.354 5.65-.342 3.148-.516 6.223-.516 9.136 0 50.735 40.881 93.221 95.093 98.821 2.698.279 4.803 2.297 5.018 4.812.216 2.515-1.522 4.817-4.139 5.487z" />
-        </svg>`;
-        document.querySelector(".cursor-icon").style.display = "block";
+        enableBackground="new 0 0 383.344 383.345"
+        height="383.345"
+        viewBox="0 0 383.344 383.345"
+        width="20px"
+        height="20px"
+        fill="#fcfcfc"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="m273.217 106.899c-27.181-44.864-57.413-83.693-73.016-102.846-2.088-2.565-5.221-4.054-8.528-4.053-3.308 0-6.44 1.489-8.529 4.054-15.602 19.159-45.834 58.001-73.015 102.869-35.028 57.823-52.789 105.63-52.789 142.09 0 74.071 60.261 134.332 134.332 134.332s134.332-60.261 134.332-134.332c.001-36.484-17.758-84.298-52.787-142.114zm-63.111 226.969c-7.844 2.006-15.986 3.022-24.205 3.022-50.186 0-91.015-37.929-91.015-84.55 0-11.255 2.97-24.405 8.825-39.083.989-2.48 3.807-3.895 6.585-3.295 2.776.598 4.64 3.018 4.354 5.65-.342 3.148-.516 6.223-.516 9.136 0 50.735 40.881 93.221 95.093 98.821 2.698.279 4.803 2.297 5.018 4.812.216 2.515-1.522 4.817-4.139 5.487z" />
+      </svg>`;
       });
       node.addEventListener("mouseleave", () => {
         document.querySelector(".cursor").style.clipPath = "circle(30%)";
         document.querySelector(".cursor-label").innerHTML = null;
-        document.querySelector(".cursor-label").style.display = "none";
         document.querySelector(".cursor-icon").innerHTML = null;
-        document.querySelector(".cursor-icon").style.display = "none";
       });
     });
   }, []);
@@ -183,18 +183,16 @@ const CursorComp = ({ popped, cursorColor }) => {
         .addEventListener("mouseenter", () => {
           document.querySelector(".cursor").style.clipPath = "circle(50%)";
           document.querySelector(".cursor-label").innerHTML = "Close";
-          document.querySelector(".cursor-label").style.display = "block";
-          document.querySelector(".cursor-icon").innerHTML = "-";
-          document.querySelector(".cursor-icon").style.display = "block";
+          document.querySelector(
+            ".cursor-icon"
+          ).innerHTML = `<span style={{ marginBottom: "5px" }}>-</span>`;
         });
       document
         .querySelector(".popout-label")
         .addEventListener("mouseleave", () => {
           document.querySelector(".cursor").style.clipPath = "circle(30%)";
           document.querySelector(".cursor-label").innerHTML = null;
-          document.querySelector(".cursor-label").style.display = "none";
           document.querySelector(".cursor-icon").innerHTML = null;
-          document.querySelector(".cursor-icon").style.display = "none";
         });
     } else {
       document
@@ -202,18 +200,16 @@ const CursorComp = ({ popped, cursorColor }) => {
         .addEventListener("mouseenter", () => {
           document.querySelector(".cursor").style.clipPath = "circle(50%)";
           document.querySelector(".cursor-label").innerHTML = "Expand";
-          document.querySelector(".cursor-label").style.display = "block";
-          document.querySelector(".cursor-icon").innerHTML = "+";
-          document.querySelector(".cursor-icon").style.display = "block";
+          document.querySelector(
+            ".cursor-icon"
+          ).innerHTML = `<span style={{ marginBottom: "4px" }}>+</span>`;
         });
       document
         .querySelector(".popout-label")
         .addEventListener("mouseleave", () => {
           document.querySelector(".cursor").style.clipPath = "circle(30%)";
           document.querySelector(".cursor-label").innerHTML = null;
-          document.querySelector(".cursor-label").style.display = "none";
           document.querySelector(".cursor-icon").innerHTML = null;
-          document.querySelector(".cursor-icon").style.display = "none";
         });
     }
   }, [popped]);
